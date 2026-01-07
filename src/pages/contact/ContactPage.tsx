1a4dce47265903d3629748b6c4135e6c7f069639
mark-makondo <markalbert.makondo@gmail.com>
2026-01-07 21:50:05 +0800

    Create contact page basic layout without functionality

--- src/pages/contact/ContactPage.tsx
+++ src/pages/contact/ContactPage.tsx
@@ -1,5 +1,23 @@
+import PageContainer from "@/common/components/shared/container/PageContainer";
+import Button from "@/common/components/ui/buttons/Button";
+import { InputText, InputEmail, Textarea } from "@/common/components/ui/inputs/Input";
+
 const ContactPage: React.FC = () => {
-    return <div className="page page--alternative page-contact">ContactPage</div>;
+    return (
+        <PageContainer className="page-contact" subtitle="Get In Touch" title="Let's Connect and Build Something Amazing!" alternative>
+            <form className="page-contant__form flex flex-col gap-4 w-full max-w-180">
+                <div className="page-content__form__top flex gap-4">
+                    <InputText placeholder="Full Name" name="name" />
+                    <InputEmail placeholder="Email Address" name="email-address" />
+                </div>
+                <InputText placeholder="Subject" name="subject" />
+                <Textarea placeholder="Message" name="message" />
+                <Button type="submit" className="btn-primary">
+                    Send Message
+                </Button>
+            </form>
+        </PageContainer>
+    );
 };
 
 export default ContactPage;
