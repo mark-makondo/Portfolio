27269afa6557dad06bfac898c1c73e260fc2b2b7
mark-makondo <markalbert.makondo@gmail.com>
2026-01-07 22:32:50 +0800

    Update logo favicon to a more compact and clean version

--- src/common/components/icons/LogoIcon.tsx
+++ src/common/components/icons/LogoIcon.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Logo from "@images/logo.svg?react";
 import LogoAlt from "@images/logo-with-bg.svg?react";
+import LogoCombined from "@images/logo-combine.svg?react";
 
 interface LogoIconProps {
     style?: React.CSSProperties;
     className?: string;
     alternative?: boolean;
+    combine?: boolean;
 }
 
-const LogoIcon: React.FC<LogoIconProps> = ({ className, alternative, ...props }) => {
-    return alternative ? <LogoAlt className={className} {...props} /> : <Logo className={className} {...props} />;
+const LogoIcon: React.FC<LogoIconProps> = ({ className, alternative, combine, ...props }) => {
+    if (combine) return <LogoCombined className={className} {...props} />;
+    if (alternative) return <LogoAlt className={className} {...props} />;
+    return <Logo className={className} {...props} />;
 };
 
 export default LogoIcon;
