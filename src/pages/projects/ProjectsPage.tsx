43e690a9d0018f6f1f83602b365365b8048464d6
mark-makondo <markalbert.makondo@gmail.com>
2026-01-19 20:40:53 +0800

    Remove default text for project page

--- src/pages/projects/ProjectsPage.tsx
+++ src/pages/projects/ProjectsPage.tsx
@@ -3,7 +3,7 @@ import PageContainer from "@/common/components/shared/container/PageContainer";
 const ProjectsPage: React.FC = () => {
     return (
         <PageContainer className="page-projects" subtitle="My Works" title="Featured Projects" alternative>
-            ProjectsPage
+            <div className="page-projects__content grow shrink basis-0 min-h-0 w-full gap-4 justify-between"></div>
         </PageContainer>
     );
 };
